67d40415ddab64ec5436e6203db371a7754eef74
avoreano <diegogodoycarvajal@gmail.com>
2025-09-10 23:50:07 -0300

    Menu and main creation

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void showMenu() {
+    cout << "=== ACADEMIC MANAGEMENT SYSTEM ===" << endl;
+    cout << "STUDENTS" << endl;
+    cout << "1. Create Student" << endl;
+    cout << "2. Find Student" << endl;
+    cout << "3. Remove Student" << endl;
+    cout << endl;
+
+    cout << "COURSES" << endl;
+    cout << "4. Create Course" << endl;
+    cout << "5. Find Course" << endl;
+    cout << "6. Remove Course" << endl;
+    cout << endl;
+
+    cout << "REGISTRATION" << endl;
+    cout << "7. Enroll student in course" << endl;
+    cout << "8. Remove student from course" << endl;
+    cout << endl;
+
+    cout << "GRADES" << endl;
+    cout << "9. Register grade" << endl;
+    cout << endl;
+
+    cout << "REPORTS" << endl;
+    cout << "10. Get all students" << endl;
+    cout << "11. Get courses from student" << endl;
+    cout << "12. Calculate course average" << endl;
+    cout << "13. Calculate overall GPA (Grade Point Average)" << endl;
+    cout << endl;
+
+    cout << "0. Exit" << endl;
+    cout << "===================================" << endl;
+    cout << "Select an option: ";
+}
+
+void runMenu() {
+    int option = -1;
+    
+    while (option != 0) {
+        showMenu();
+        cin >> option;
+
+        if (option == 1) {
+            cout << "=== CREATE STUDENT ===" << endl;
+            // function to create student
+        }
+        else if (option == 2) {
+            cout << "=== FIND STUDENT ===" << endl;
+            // function to find student
+        }
+        else if (option == 3) {
+            cout << "=== REMOVE STUDENT ===" << endl;
+            // function to remove student
+        }
+        else if (option == 4) {
+            cout << "=== CREATE COURSE ===" << endl;
+            // function to create course
+        }
+        else if (option == 5) {
+            cout << "=== FIND COURSE ===" << endl;
+            // function to find course
+        }
+        else if (option == 6) {
+            cout << "=== REMOVE COURSE ===" << endl;
+            // function to remove course
+        }
+        else if (option == 7) {
+            cout << "=== ENROLL STUDENT IN COURSE ===" << endl;
+            // function to enroll student in course
+        }
+        else if (option == 8) {
+            cout << "=== REMOVE STUDENT FROM COURSE ===" << endl;
+            // function to remove student from course
+        }
+        else if (option == 9) {
+            cout << "=== REGISTER GRADE ===" << endl;
+            // function to register grade
+        }
+        else if (option == 10) {
+            cout << "=== GET ALL STUDENTS ===" << endl;
+            // function to get all students
+        }
+        else if (option == 11) {
+            cout << "=== GET COURSES FROM STUDENT ===" << endl;
+            // function to get courses from student
+        }
+        else if (option == 12) {
+            cout << "=== CALCULATE COURSE AVERAGE ===" << endl;
+            // function to calculate course average
+        }
+        else if (option == 13) {
+            cout << "=== CALCULATE OVERALL GPA ===" << endl;
+            // function to calculate overall GPA
+        }
+        else if (option == 0) {
+            cout << "Exiting the program." << endl;
+        }
+        else {
+            cout << "Invalid option. Please try again." << endl;
+        }
+    }
+}
+
+int main() {
+    runMenu();
+    return 0;
+}
